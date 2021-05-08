7abe24c1024a5576f0841f5f4b850ea841984ddc
katastrophical <britorojas.sebastian@gmail.com>
2021-05-07 20:41:15 -0400

    DOCS creado

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebPackPlugin       = require('html-webpack-plugin');
 const MiniCssExtractPlugin    = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
+
 module.exports = {
     mode: 'development',
     optimization: {
